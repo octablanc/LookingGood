3127e77164c30166ff8aa58faf0637fcf186e30c
octablanc <octavioblanc234@gmail.com>
2022-11-09 23:14:46 -0300

    Daily commit

--- client/src/components/styles/RecipeCardStyle.jsx
+++ client/src/components/styles/RecipeCardStyle.jsx
@@ -11,7 +11,6 @@ const RecipeCardS = styled.div`
     border-radius: 5px;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
     
-    
     .picture {
         background-size: 1000px;
         border-top-left-radius: 5px;
